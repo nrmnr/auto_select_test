1ddb3946347f3d6257e73ba78b5ed290dd32df1c
n-fujita <n-fujita@daiichi-g.co.jp>
2012-11-20 21:13:01 +0900

    バックトラック情報用クラス作成

--- autosel.cpp
+++ autosel.cpp
@@ -1,8 +1,11 @@
 #include "autosel.h"
 #include <algorithm>
+#include "assert.h"
 
 int AutoSelect::detect(Nodes& selected, const Nodes& candidate, const Qids& qids, int count_req)
 {
+	m_min_overlap = calc_min_overlap(candidate, qids.size() * count_req);
+
 	m_candidate.assign(candidate.begin(), candidate.end());
 	std::random_shuffle(m_candidate.begin(), m_candidate.end());
 
@@ -10,8 +13,20 @@ int AutoSelect::detect(Nodes& selected, const Nodes& candidate, const Qids& qids
 
 	init_req_count(count_req);
 
-	m_selected.clear();
-	int result = detect_comb(0);
+	SelectInfo info;
+	init_select_info(info);
+
+	int result = detect_comb(info, 0);
+
+	selected.clear();
+	for (SelectInfo::const_iterator si = info.begin(); si != info.end(); ++si) {
+		Qid qid = si->first;
+		Kids kids;
+		si->second.get(kids);
+		for (Kids::iterator ki = kids.begin(); ki != kids.end(); ++ki) {
+			selected.push_back(Node(qid, *ki));
+		}
+	}
 
 	selected.assign(m_selected.begin(), m_selected.end());
 	return result;
@@ -25,11 +40,26 @@ void AutoSelect::init_req_count(int count_req)
 	}
 }
 
-int AutoSelect::detect_comb(int pos)
+void AutoSelect::init_select_info(SelectInfo& info)
+{
+	assert(m_req_per_qid.size() > 0);
+}
+
+int AutoSelect::calc_min_overlap(const Nodes& candidate, int total_count_req) const
+{
+	std::set<Kid> kidset;
+	for (Nodes::const_iterator ni = candidate.begin(); ni != candidate.end(); ++ni) {
+		kidset.insert(ni->kid);
+	}
+	int count_kid = kidset.size();
+	return (count_kid <= total_count_req)? 0 : (count_kid - total_count_req);
+}
+
+int AutoSelect::detect_comb(SelectInfo& info, int pos)
 {
 	if (pos >= m_candidate.size()) {
-		return 0;
+		return INT_MAX;
 	}
 	m_selected.push_back(m_candidate[pos]);
-	return detect_comb(pos+1);
+	return detect_comb(info, pos+1);
 }
--- autosel.h
+++ autosel.h
@@ -3,18 +3,49 @@
 #include <map>
 
 typedef std::map<Qid,int> CountPerQid;
+class QidSelectInfo
+{
+ public:
+	QidSelectInfo(const Qid& qid, int req)
+		: qid(qid), req(req)
+	{}
+	bool operator<(const QidSelectInfo& r) const {
+		return qid < r.qid;
+	}
+	bool done() const {
+		return (kidset.size() == req);
+	}
+	void push(const Kid& kid) {
+		kidset.insert(kid);
+	}
+	void pop(const Kid& kid) {
+		kidset.erase(kid);
+	}
+	void get(Kids& kids) const {
+		kids.assign(kidset.begin(), kidset.end());
+	}
+ private:
+	Qid qid;
+	int req;
+	std::set<Kid> kidset;
+};
+typedef std::map<Qid, QidSelectInfo> SelectInfo;
+
 
 class AutoSelect
 {
  public:
+
 	AutoSelect(){}
 	virtual ~AutoSelect(){}
 
 	int detect(Nodes& selected, const Nodes& candidate, const Qids& qids, int count_req);
 
  protected:
-	int detect_comb(int pos);
+	int detect_comb(SelectInfo& info, int pos);
 	void init_req_count(int count_req);
+	void init_select_info(SelectInfo& info);
+	int calc_min_overlap(const Nodes& candidate, int total_count_req) const;
 
  private:
 	Nodes m_selected;
@@ -22,4 +53,5 @@ class AutoSelect
 	Qids m_require_qids;
 
 	CountPerQid m_req_per_qid;
+	int m_min_overlap;
 };
